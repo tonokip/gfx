b4337464c299936b5b12d8682625e9f2be9d3119
Ed <http://support.microchip.com>
2018-07-16 14:38:08 -0700

    Add aria_showcase_reloaded GFX application

---
+++ apps/aria_showcase_reloaded/firmware/src/config/sam_e70_xplained_ultra_wqvga_lut8/gfx/utils/gfxu_config.h
@@ -0,0 +1,11 @@
+#ifndef GFXU_CONFIG_H
+#define GFXU_CONFIG_H
+
+#ifndef LIB_EXPORT
+#define LIB_EXPORT
+#endif
+
+#define GFX_UTIL_JPEG_DECODER_ENABLED       0
+#define GFX_UTIL_PNG_DECODER_ENABLED       0
+
+#endif /* GFXU_CONFIG_H */
