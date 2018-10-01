0aa7bbd6c66e47368567ea8ca1fb9cbb9c1c83e8
Ed <http://support.microchip.com>
2018-10-01 15:20:26 -0700

    Update GFX template to define display interface control pins

--- templates/aria_gfx_xplained_pro/Support_BSP_SAM_E70_Xplained_Ultra.py
+++ templates/aria_gfx_xplained_pro/Support_BSP_SAM_E70_Xplained_Ultra.py
@@ -1,13 +1,13 @@
 spi4PinComponentIDList = ["spi0", "drv_spi", "drv_spi_0"]
 spi4PinAutoConnectList = [["drv_spi_0", "drv_spi_SPI_dependency", "spi0", "SPI0_SPI"],
 							["gfx_driver_ili9488", "DRV_SPI", "drv_spi_0", "drv_spi"]]
-spi4PinConfigs = [{"pin": 31, "name": "BSP_ILI9488_SPI_DCX", "type": "GPIO", "direction": "Out", "latch": "High"},
-				{"pin": 52, "name": "BSP_ILI9488_SPI_CSX", "type": "GPIO", "direction": "Out", "latch": "High"},
+spi4PinConfigs = [{"pin": 31, "name": "GFX_DISP_INTF_PIN_RSDC", "type": "GPIO", "direction": "Out", "latch": "High"},
+				{"pin": 52, "name": "GFX_DISP_INTF_PIN_CS", "type": "GPIO", "direction": "Out", "latch": "High"},
 				{"pin": 60, "name": "SPI0_SPCK", "type": "SPI0_SPCK", "direction": "", "latch": ""},
 				{"pin": 63, "name": "SPI0_MOSI", "type": "SPI0_MOSI", "direction": "", "latch": ""},
 				{"pin": 65, "name": "SPI0_MISO", "type": "SPI0_MISO", "direction": "", "latch": ""},
-				{"pin": 102, "name": "BSP_DisplayBacklight", "type": "GPIO", "direction": "Out", "latch": "High"},
-				{"pin": 103, "name": "BSP_DisplayReset", "type": "GPIO", "direction": "Out", "latch": "High"}]
+				{"pin": 102, "name": "GFX_DISP_INTF_PIN_BACKLIGHT", "type": "GPIO", "direction": "Out", "latch": "High"},
+				{"pin": 103, "name": "GFX_DISP_INTF_PIN_RESET", "type": "GPIO", "direction": "Out", "latch": "High"}]
 
 parallelComponentIDList = ["smc"]
 parallelAutoConnectList = [["gfx_driver_ili9488", "SMC_CS", "smc", "smc_cs0"]]
@@ -17,7 +17,7 @@ ParallelPinConfigs = [{"pin": 4, "name": "EBI_D8", "type": "EBI_D8", "direction"
 					{"pin": 10, "name": "EBI_D11", "type": "EBI_D11", "direction": "", "latch": ""},
 					{"pin": 11, "name": "EBI_D0", "type": "EBI_D0", "direction": "", "latch": ""},
 					{"pin": 15, "name": "EBI_A12", "type": "EBI_A12", "direction": "", "latch": ""},
-					{"pin": 19, "name": "BSP_DisplayReset", "type": "GPIO", "direction": "Out", "latch": "High"},
+					{"pin": 19, "name": "GFX_DISP_INTF_PIN_RESET", "type": "GPIO", "direction": "Out", "latch": "High"},
 					{"pin": 27, "name": "EBI_D12", "type": "EBI_D12", "direction": "", "latch": ""},
 					{"pin": 28, "name": "EBI_D13", "type": "EBI_D13", "direction": "", "latch": ""},
 					{"pin": 38, "name": "EBI_D1", "type": "EBI_D1", "direction": "", "latch": ""},
@@ -29,9 +29,9 @@ ParallelPinConfigs = [{"pin": 4, "name": "EBI_D8", "type": "EBI_D8", "direction"
 					{"pin": 49, "name": "EBI_D14", "type": "EBI_D14", "direction": "", "latch": ""},
 					{"pin": 54, "name": "EBI_D6", "type": "EBI_D6", "direction": "", "latch": ""},
 					{"pin": 58, "name": "EBI_D5", "type": "EBI_D5", "direction": "", "latch": ""},
-					{"pin": 67, "name": "BSP_ILI9488_NCS", "type": "GPIO", "direction": "Out", "latch": "High"},
+					{"pin": 67, "name": "GFX_DISP_INTF_PIN_CS", "type": "GPIO", "direction": "Out", "latch": "High"},
 					{"pin": 82, "name": "EBI_NWR0/NWE", "type": "EBI_NWR0/NWE", "direction": "", "latch": ""},
-					{"pin": 86, "name": "BSP_DisplayBacklight", "type": "GPIO", "direction": "Out", "latch": "High"},
+					{"pin": 86, "name": "GFX_DISP_INTF_PIN_BACKLIGHT", "type": "GPIO", "direction": "Out", "latch": "High"},
 					{"pin": 94, "name": "EBI_NRD", "type": "EBI_NRD", "direction": "", "latch": ""}]
 
 					
