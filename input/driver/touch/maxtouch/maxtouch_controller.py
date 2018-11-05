ca8e2b29cb5807872551738f1b125a9c9ff1f059
Ed <http://support.microchip.com>
2018-11-05 15:55:21 -0700

    MH3-1738 - Add RTOS support for maXTouch Input Driver

--- input/driver/touch/maxtouch/maxtouch_controller.py
+++ input/driver/touch/maxtouch/maxtouch_controller.py
@@ -41,13 +41,13 @@ def instantiateComponent(component):
 	projectPath = "config/" + Variables.get("__CONFIGURATION_NAME") + "/driver/input"
 	
 	DRV_MAXTOUCH_H = component.createFileSymbol("DRV_MAXTOUCH_H", None)
-	DRV_MAXTOUCH_H.setSourcePath("drv_maxtouch.h")
+	DRV_MAXTOUCH_H.setSourcePath("inc/drv_maxtouch.h")
 	DRV_MAXTOUCH_H.setDestPath("driver/input/")
 	DRV_MAXTOUCH_H.setProjectPath(projectPath)
 	DRV_MAXTOUCH_H.setType("HEADER")
 	
 	DRV_MAXTOUCH_C = component.createFileSymbol("DRV_MAXTOUCH_C", None)
-	DRV_MAXTOUCH_C.setSourcePath("drv_maxtouch.c")
+	DRV_MAXTOUCH_C.setSourcePath("src/drv_maxtouch.c")
 	DRV_MAXTOUCH_C.setDestPath("driver/input/")
 	DRV_MAXTOUCH_C.setProjectPath(projectPath)
 	DRV_MAXTOUCH_C.setType("SOURCE")
@@ -57,33 +57,79 @@ def instantiateComponent(component):
 	I2CIndex.setDefaultValue(0)
 	I2CIndex.setMin(0)
 	
-	SysDefines = component.createFileSymbol("SysDefines", None)
-	SysDefines.setType("STRING")
-	SysDefines.setOutputName("core.LIST_SYSTEM_CONFIG_H_DRIVER_CONFIGURATION")
-	SysDefines.setSourcePath("drv_maxtouch_defines.ftl")
-	SysDefines.setMarkup(True)
+	RTOSMenu = component.createMenuSymbol("RTOSMenu", None)
+	RTOSMenu.setLabel("RTOS Settings")
+	RTOSMenu.setDescription("RTOS Settings")
+	RTOSMenu.setVisible(Database.getSymbolValue("HarmonyCore", "SELECT_RTOS") != "BareMetal")
+	RTOSMenu.setDependencies(showTouchRTOSMenu, ["HarmonyCore.SELECT_RTOS"])
 	
-	SysInitData = component.createFileSymbol("SysInitData", None)
-	SysInitData.setType("STRING")
-	SysInitData.setOutputName("core.LIST_SYSTEM_INIT_C_DRIVER_INITIALIZATION_DATA")
-	SysInitData.setSourcePath("drv_maxtouch_init.ftl")
-	SysInitData.setMarkup(True)
-	
-	SysObjString = component.createListEntrySymbol("SysObjString", None)
-	SysObjString.addValue('    SYS_MODULE_OBJ  drvMAXTOUCH;')
-	SysObjString.setTarget("core.LIST_SYSTEM_DEFINITIONS_H_OBJECTS")
-	
-	SysTasksString = component.createListEntrySymbol("SYSTasksString", None)
-	SysTasksString.addValue("DRV_MAXTOUCH_Tasks(sysObj.drvMAXTOUCH);")
-	SysTasksString.setTarget("core.LIST_SYSTEM_TASKS_C_CALL_DRIVER_TASKS")
-	
-	SysInitString = component.createListEntrySymbol("SysInitString", None)
-	SysInitString.addValue("    sysObj.drvMAXTOUCH = DRV_MAXTOUCH_Initialize(0, (SYS_MODULE_INIT *)&drvMAXTOUCHInitData);")
-	SysInitString.setTarget("core.LIST_SYSTEM_INIT_C_SYS_INITIALIZE_DRIVERS")
+	RTOSEnabled = component.createBooleanSymbol("RTOSEnabled", RTOSMenu)
+	RTOSEnabled.setLabel("RTOS Support Enabled")
+	RTOSEnabled.setDefaultValue(Database.getSymbolValue("HarmonyCore", "SELECT_RTOS") != "BareMetal")
+	RTOSEnabled.setDependencies(enableTouchRTOSymbol, ["HarmonyCore.SELECT_RTOS"])
+	RTOSEnabled.setVisible(False)
 
-	SysInitIncludeString = component.createListEntrySymbol("SysInitIncludeString", None)
-	SysInitIncludeString.addValue('#include "driver/input/drv_maxtouch.h"')
-	SysInitIncludeString.setTarget("core.LIST_SYSTEM_CONFIG_H_GLOBAL_INCLUDES")
+	RTOSStackSize = component.createIntegerSymbol("RTOSStackSize", RTOSMenu)
+	RTOSStackSize.setLabel("Stack Size")
+	RTOSStackSize.setMin(0)
+	RTOSStackSize.setDefaultValue(1024)
+	
+	RTOSTaskPriority = component.createIntegerSymbol("RTOSTaskPriority", RTOSMenu)
+	RTOSTaskPriority.setLabel("Task Priority")
+	RTOSTaskPriority.setDefaultValue(1)
+	
+	RTOSUseDelay = component.createBooleanSymbol("RTOSUseDelay", RTOSMenu)
+	RTOSUseDelay.setLabel("Use Task Delay?")
+	RTOSUseDelay.setDefaultValue(True)
+	
+	RTOSTaskDelay = component.createIntegerSymbol("RTOSTaskDelay", RTOSMenu)
+	RTOSTaskDelay.setLabel("Task Delay")
+	RTOSTaskDelay.setDefaultValue(10)
+	RTOSTaskDelay.setMin(0)
+	
+	TOUCH_DRV_CONFIG_H = component.createFileSymbol("TOUCH_DRV_CONFIG_H", None)
+	TOUCH_DRV_CONFIG_H.setType("STRING")
+	TOUCH_DRV_CONFIG_H.setOutputName("core.LIST_SYSTEM_CONFIG_H_DRIVER_CONFIGURATION")
+	TOUCH_DRV_CONFIG_H.setSourcePath("templates/system/drv_maxtouch_defines.ftl")
+	TOUCH_DRV_CONFIG_H.setMarkup(True)
+	
+	TOUCH_DRV_INIT_DATA = component.createFileSymbol("TOUCH_DRV_INIT_DATA", None)
+	TOUCH_DRV_INIT_DATA.setType("STRING")
+	TOUCH_DRV_INIT_DATA.setOutputName("core.LIST_SYSTEM_INIT_C_DRIVER_INITIALIZATION_DATA")
+	TOUCH_DRV_INIT_DATA.setSourcePath("templates/system/drv_maxtouch_init.ftl")
+	TOUCH_DRV_INIT_DATA.setMarkup(True)
+	
+	TOUCH_DRV_OBJ_DATA = component.createFileSymbol("TOUCH_DRV_OBJ_DATA", None)
+	TOUCH_DRV_OBJ_DATA.setType("STRING")
+	TOUCH_DRV_OBJ_DATA.setOutputName("core.LIST_SYSTEM_DEFINITIONS_H_OBJECTS")
+	TOUCH_DRV_OBJ_DATA.setSourcePath("templates/system/drv_maxtouch_definitions_obj.h.ftl")
+	TOUCH_DRV_OBJ_DATA.setMarkup(True)
+
+	TOUCH_DRV_DEFINITIONS_H = component.createFileSymbol("TOUCH_DRV_DEFINITIONS_H", None)
+	TOUCH_DRV_DEFINITIONS_H.setType("STRING")
+	TOUCH_DRV_DEFINITIONS_H.setOutputName("core.LIST_SYSTEM_DEFINITIONS_H_INCLUDES")
+	TOUCH_DRV_DEFINITIONS_H.setSourcePath("templates/system/drv_maxtouch_definitions.h.ftl")
+	TOUCH_DRV_DEFINITIONS_H.setMarkup(True)
+	
+	TOUCH_DRV_INIT_C = component.createFileSymbol("TOUCH_DRV_INIT_C", None)
+	TOUCH_DRV_INIT_C.setType("STRING")
+	TOUCH_DRV_INIT_C.setOutputName("core.LIST_SYSTEM_INIT_C_SYS_INITIALIZE_DRIVERS")
+	TOUCH_DRV_INIT_C.setSourcePath("templates/system/drv_maxtouch_initialize.c.ftl")
+	TOUCH_DRV_INIT_C.setMarkup(True)
+
+	TOUCH_DRV_TASK_C = component.createFileSymbol("TOUCH_DRV_TASK_C", None)
+	TOUCH_DRV_TASK_C.setType("STRING")
+	TOUCH_DRV_TASK_C.setOutputName("core.LIST_SYSTEM_TASKS_C_CALL_DRIVER_TASKS")
+	TOUCH_DRV_TASK_C.setSourcePath("templates/system/drv_maxtouch_tasks.c.ftl")
+	TOUCH_DRV_TASK_C.setMarkup(True)
+
+	TOUCH_DRV_RTOS_TASK_C = component.createFileSymbol("TOUCH_DRV_RTOS_TASK_C", None)
+	TOUCH_DRV_RTOS_TASK_C.setType("STRING")
+	TOUCH_DRV_RTOS_TASK_C.setOutputName("core.LIST_SYSTEM_RTOS_TASKS_C_DEFINITIONS")
+	TOUCH_DRV_RTOS_TASK_C.setSourcePath("templates/system/drv_maxtouch_rtos_tasks.c.ftl")
+	TOUCH_DRV_RTOS_TASK_C.setMarkup(True)
+	TOUCH_DRV_RTOS_TASK_C.setEnabled((Database.getSymbolValue("HarmonyCore", "SELECT_RTOS") != "BareMetal"))
+	TOUCH_DRV_RTOS_TASK_C.setDependencies(enableTouchRTOSymbol, ["HarmonyCore.SELECT_RTOS"])
 	
 def onAttachmentConnected(source, target):
 	#print(component)
@@ -106,3 +152,15 @@ def onAttachmentDisconnected(source, target):
 		I2CIndex = source["component"].getSymbolByID("I2CIndex")
 		I2CIndex.clearValue()
 		I2CIndex.setReadOnly(False)
+		
+def enableTouchRTOSymbol(symbol, event):
+    if (event["value"] != "BareMetal"):
+        symbol.setEnabled(True)
+    else:
+        symbol.setEnabled(False)
+		
+def showTouchRTOSMenu(symbol, event):
+	if (event["value"] != "BareMetal"):
+		symbol.setVisible(True)
+	else:
+		symbol.setVisible(False)
