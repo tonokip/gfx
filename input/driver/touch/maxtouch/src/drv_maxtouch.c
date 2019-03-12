d4dc403f2adf7c733f1c4920f16cfbc73e1e0dfe
Bill <http://support.microchip.com>
2019-03-11 17:02:42 -0700

    MH3-8186: Missing null character initialization

--- input/driver/touch/maxtouch/src/drv_maxtouch.c
+++ input/driver/touch/maxtouch/src/drv_maxtouch.c
@@ -2392,8 +2392,8 @@ bool mxt_configure_objects(struct DEVICE_OBJECT* pDeviceObject, DRV_MAXTOUCH_Fir
 bool mxt_load_xcfg_file(struct DEVICE_OBJECT * pDeviceObject, const char *filename)
 {
     char c;
-    char item[255];
-	char readline[255] = {''};
+	char item[255] = { '\0' };
+	char readline[255] = {'\0'};
     char *substr;
     int instance;
     int object_address;
