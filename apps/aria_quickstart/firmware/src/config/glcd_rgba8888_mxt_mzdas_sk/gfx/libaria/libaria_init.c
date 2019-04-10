3228250610b66af19263248b0f9350cc62f9bfc1
Ed <http://support.microchip.com>
2019-04-10 10:11:40 -0700

    aria_quickstart - regenerate aria_qs_mzda_intddr_sk_meb2_tm4301b

--- apps/aria_quickstart/firmware/src/config/glcd_rgba8888_mxt_mzdas_sk/gfx/libaria/libaria_init.c
+++ apps/aria_quickstart/firmware/src/config/glcd_rgba8888_mxt_mzdas_sk/gfx/libaria/libaria_init.c
@@ -92,7 +92,7 @@ static void ScreenCreate_screen1(laScreen* screen)
     laWidget_SetSize((laWidget*)layer0, 480, 272);
     laWidget_SetBackgroundType((laWidget*)layer0, LA_WIDGET_BACKGROUND_FILL);
     laWidget_SetScheme((laWidget*)layer0, &NewScheme);
-    laLayer_SetBufferCount(layer0, 2);
+    laLayer_SetBufferCount(layer0, 1);
 
     laScreen_SetLayer(screen, 0, layer0);
 
