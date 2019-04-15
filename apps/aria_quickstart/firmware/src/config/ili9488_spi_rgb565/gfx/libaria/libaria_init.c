d968340cdbe6e9dd00a84949d30562545136b32f
Ed <http://support.microchip.com>
2019-04-15 13:38:36 -0700

    Regenerate aria_qs_e70_xu_xpro_spi to tip

--- apps/aria_quickstart/firmware/src/config/ili9488_spi_rgb565/gfx/libaria/libaria_init.c
+++ apps/aria_quickstart/firmware/src/config/ili9488_spi_rgb565/gfx/libaria/libaria_init.c
@@ -40,6 +40,7 @@
 // DOM-IGNORE-END
 
 #include "gfx/libaria/libaria_init.h"
+#include "gfx/libaria/inc/libaria_utils.h"
 
 laImageWidget* LogoWidget;
 laLabelWidget* TitleLabel;
@@ -101,6 +102,7 @@ static void ScreenCreate_screen1(laScreen* screen)
     laWidget_AddChild((laWidget*)layer0, (laWidget*)SloganButton);
 
 }
+
 
 
 
